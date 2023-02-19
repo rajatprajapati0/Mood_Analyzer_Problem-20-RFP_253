fa5a30a78b8ba898559890e9d52aad9a1c1446cd
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-19 21:06:28 +0530

    Modified:TC1.2 Test case pass analyseMood method need to check for Sad else return HAPPY

--- Mood_Analyzer/Mood.cs
+++ Mood_Analyzer/Mood.cs
@@ -22,7 +22,7 @@ namespace Mood_Analyzer
             }
             else
             {
-                mymood = null;
+                mymood = "HAPPY";
             }
 
 
--- Test_Mood_Analayzer/UnitTest1.cs
+++ Test_Mood_Analayzer/UnitTest1.cs
@@ -17,12 +17,24 @@ namespace Test_Mood_Analayzer
         [TestCase("i am sad")]
         [TestCase("i am in sad mood")]
         [TestCase("I AM SAD")]
-        public void test_TO_checkMood(string moodinput)
+        public void test_TO_checkMood_FOR_SAD(string moodinput)
         {
              mood= new Mood();
             string expected = "SAD";
             var actual=mood.checkMood(moodinput);
             Assert.AreEqual(expected,actual);
+        }
+        [TestCase("happy")]
+        [TestCase("HAPPY")]
+        [TestCase("i am happy")]
+        [TestCase("i am not in sad mood")]
+        [TestCase("I AM in any mood")]
+        public void test_TO_checkMood_FOR_HAPPY(string moodinput)
+        {
+            mood = new Mood();
+            string expected = "HAPPY";
+            var actual = mood.checkMood(moodinput);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
