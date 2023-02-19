f9a65abf14fd23460a40a2d4cc4557689134717e
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-19 19:19:21 +0530

    Adedd:Mood Analyzer program

---
+++ Mood_Analyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mood_Analyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
